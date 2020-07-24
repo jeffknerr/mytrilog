959d0613be7061e87b7b20457844f676ae6ae4d6
Jeff Knerr <knerr@goofydogears.net>
2020-07-24 19:55:47 -0400

    fixed up favicon, HOME bugs

--- app/__init__.py
+++ app/__init__.py
@@ -17,6 +17,7 @@ login.login_view = 'auth.login'
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
+PREFIX = "/mytrilog"
 
 def create_app(config_class=Config):
   app = Flask(__name__)
@@ -30,11 +31,11 @@ def create_app(config_class=Config):
   moment.init_app(app)
 
   from app.errors import bp as errors_bp
-  app.register_blueprint(errors_bp)
+  app.register_blueprint(errors_bp, url_prefix=PREFIX)
   from app.auth import bp as auth_bp
-  app.register_blueprint(auth_bp, url_prefix='/auth')
+  app.register_blueprint(auth_bp, url_prefix=PREFIX+'/auth')
   from app.main import bp as main_bp
-  app.register_blueprint(main_bp)
+  app.register_blueprint(main_bp, url_prefix=PREFIX)
 
   if not app.debug and not app.testing:
       if app.config['MAIL_SERVER']:
--- config.py
+++ config.py
@@ -5,21 +5,6 @@ from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
-def getAddresses():
-  """get the admin email addresses from a file"""
-  emails = []
-  HOME = os.environ["HOME"]
-  filename = "%s/.admin_emails" % (HOME)
-  try:
-    inf = open(filename,"r")
-    for line in inf:
-      if not line.startswith("#"):
-        emails.append(line.strip())
-    inf.close()
-  except FileNotFoundError:
-    print("No .admin_emails file found...")
-  return emails
-
 class Config(object):
   SECRET_KEY = os.environ.get('SECRET_KEY') or 'really-secret-key'
   SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir,'app.db')
@@ -29,5 +14,5 @@ class Config(object):
   MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
   MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
   MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-  ADMINS = getAddresses()
+  ADMINS = os.environ.get('ADMINS')
   WORKOUTS_PER_PAGE = 5
