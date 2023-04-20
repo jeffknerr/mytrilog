9fde2fbb12cda506e61daf24390f3aba62e3483d
Jeffrey M. Knerr <knerr@cs.swarthmore.edu>
2023-04-20 14:10:45 -0400

    more tests, better coverage

--- tests/functional/test_auth.py
+++ tests/functional/test_auth.py
@@ -40,16 +40,11 @@ def test_log_in_as_user(client, auth, app_with_user):
     assert bhstr in response.data
     assert b"Log a workout?" in response.data
     assert b"Sign In" not in response.data
+    # now log out
+    response = auth.logout()
 
 
 def test_logout_user(client, auth, app_with_user):
-    """
-    GIVEN a Flask application configured for testing
-    WHEN the user is log in with VALID data
-    THEN check that existing user is successfully login out
-         check that there was one redirect response
-         check that user is successfully redirected to index page
-    """
     # first log in
     response = auth.login(uname, pw)
     assert response.status_code == 200
@@ -61,12 +56,198 @@ def test_logout_user(client, auth, app_with_user):
     # now log out
     response = auth.logout()
     assert response.status_code == 200
-    # should go straight to /mytrilog/auth/login
+    # should go straight to /mytrilog/auth/login, so only 1 item in list
     assert len(response.history) == 1
 #   print(response.history[0].data)
     assert response.request.path == "/mytrilog/auth/login"
 
+def test_about(client, auth, app_with_user):
+    # first log in
+    response = auth.login(uname, pw)
+    assert response.status_code == 200
+    hstr = "Hello, %s!" % uname
+    bhstr = hstr.encode('utf-8')
+    assert bhstr in response.data
+    # now go to About page
+    response = client.get("/mytrilog/about")
+    assert response.status_code == 200
+    bstr = "This is the start of Jeff's training log app...".encode('utf-8')
+    assert bstr in response.data
+    # now log out
+    response = auth.logout()
 
+def test_profile(client, auth, app_with_user):
+    # first log in
+    response = auth.login(uname, pw)
+    assert response.status_code == 200
+    hstr = "Hello, %s!" % uname
+    bhstr = hstr.encode('utf-8')
+    assert bhstr in response.data
+    # now go to Profile page
+    response = client.get("/mytrilog/user/%s" % uname)
+    assert response.status_code == 200
+    ustr = "User: %s" % uname
+    bstr = ustr.encode('utf-8')
+    assert bstr in response.data
+    bstr = "Last seen on:".encode('utf-8')
+    assert bstr in response.data
+    bstr = "Edit your profile".encode('utf-8')
+    assert bstr in response.data
+    # try one that doesn't exist
+    response = client.get("/mytrilog/user/sdkjfsjflkj")
+    assert response.status_code == 404
+    # now log out
+    response = auth.logout()
+
+def test_stats(client, auth, app_with_user):
+    # first log in
+    response = auth.login(uname, pw)
+    assert response.status_code == 200
+    hstr = "Hello, %s!" % uname
+    bhstr = hstr.encode('utf-8')
+    assert bhstr in response.data
+    # now go to Stats page
+    response = client.get("/mytrilog/stats")
+    assert response.status_code == 200
+    bstr = "Your Statistics".encode('utf-8')
+    assert bstr in response.data
+    bstr = "for the last 30 days of data...".encode('utf-8')
+    assert bstr in response.data
+    # now log out
+    response = auth.logout()
+
+def test_ytd(client, auth, app_with_user):
+    # first log in
+    response = auth.login(uname, pw)
+    assert response.status_code == 200
+    hstr = "Hello, %s!" % uname
+    bhstr = hstr.encode('utf-8')
+    assert bhstr in response.data
+    # now go to YTD page
+    response = client.get("/mytrilog/ytd")
+    assert response.status_code == 200
+    bstr = "your year-to-date data...".encode('utf-8')
+    assert bstr in response.data
+    # now log out
+    response = auth.logout()
+
+def test_plots(client, auth, app_with_user):
+    # first log in
+    response = auth.login(uname, pw)
+    assert response.status_code == 200
+    hstr = "Hello, %s!" % uname
+    bhstr = hstr.encode('utf-8')
+    assert bhstr in response.data
+    # now go to plot pages (should work/be ok)
+    response = client.get("/mytrilog/plot/%s" % uname)
+    assert response.status_code == 200
+    response = client.get("/mytrilog/ytdplot/%s" % uname)
+    assert response.status_code == 200
+    # try non-existent ones (if logged in, should redit to user/uname)
+    response = client.get("/mytrilog/plot/%s" % uname+"sdfsdfdf")
+    assert response.status_code == 302
+    response = client.get("/mytrilog/ytdplot/%s" % uname+"sdfsdfdf")
+    assert response.status_code == 302
+    # now log out
+    response = auth.logout()
+
+def test_unloggedin_user(client, auth, app_with_user):
+    # since we have app_with_user as param, should have a valid
+    # but non-logged in user already, and all of these should
+    # just redirect back to login page
+    response = client.get("/mytrilog/plot/%s" % uname)
+    assert response.status_code == 302
+    response = client.get("/mytrilog/ytdplot/%s" % uname)
+    assert response.status_code == 302
+    response = client.get("/mytrilog/user/%s" % uname)
+    assert response.status_code == 302
+    response = client.get("/mytrilog/ytd")
+    assert response.status_code == 302
+    response = client.get("/mytrilog/edit")
+    assert response.status_code == 302
+    response = client.get("/mytrilog/explore")
+    assert response.status_code == 302
+
+def test_log_a_workout(client, auth, app_with_user):
+    # valid user login
+    response = auth.login(uname, pw)
+    assert response.status_code == 200
+    hstr = "Hello, %s!" % uname
+    bhstr = hstr.encode('utf-8')
+    assert bhstr in response.data
+    # now add a workout
+    response = client.post("/mytrilog/index",
+            data={'what': what, 'when': wdate,
+                'amount': amt, 'weight': wgt,
+                'comment': cmt},
+            follow_redirects=True
+            )
+    assert response.status_code == 200
+    hstr = "Hello, %s!" % uname
+    bhstr = hstr.encode('utf-8')
+    assert bhstr in response.data
+    bstr = "Logged your workout!".encode('utf-8')
+    assert bstr in response.data
+    bstr = what.encode('utf-8')
+    assert bstr in response.data
+    bstr = cmt.encode('utf-8')
+    assert bstr in response.data
+    # now log out
+    response = auth.logout()
+
+def test_explore(client, auth, app_with_user):
+    # valid user login
+    response = auth.login(uname, pw)
+    assert response.status_code == 200
+    hstr = "Hello, %s!" % uname
+    bhstr = hstr.encode('utf-8')
+    assert bhstr in response.data
+    # now add a workout
+    response = client.post("/mytrilog/index",
+            data={'what': what, 'when': wdate,
+                'amount': amt, 'weight': wgt,
+                'comment': cmt},
+            follow_redirects=True
+            )
+    assert response.status_code == 200
+    hstr = "Hello, %s!" % uname
+    bhstr = hstr.encode('utf-8')
+    assert bhstr in response.data
+    bstr = "Logged your workout!".encode('utf-8')
+    assert bstr in response.data
+    bstr = what.encode('utf-8')
+    assert bstr in response.data
+    bstr = cmt.encode('utf-8')
+    assert bstr in response.data
+    # now log out
+    response = auth.logout()
+    # register another user
+    u2 = "Lily"
+    e2 = "lily@example.com"
+    p2 = "woofwoofbarkbark"
+    response = client.post("/mytrilog/auth/register",
+            data={'username': u2, 'email': e2,
+                'password': p2, 'password2': p2},
+            follow_redirects=True
+            )
+    assert response.status_code == 200
+    # log them in
+    response = auth.login(u2, p2)
+    assert response.status_code == 200
+    # go to explore
+    response = client.get("/mytrilog/explore")
+    assert response.status_code == 200
+    # should be able to see first user's workout
+    bstr = what.encode('utf-8')
+    assert bstr in response.data
+    bstr = cmt.encode('utf-8')
+    assert bstr in response.data
+    bstr = uname.encode('utf-8')
+    assert bstr in response.data
+    # now log out
+    response = auth.logout()
+
+# -------------------------------------------
 # https://testdriven.io/blog/flask-pytest/
 
 def test_login_page(test_client):
