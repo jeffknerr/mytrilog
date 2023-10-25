4957e7de074bd477067ca312fb41219444d172c8
Jeffrey M. Knerr <knerr@cs.swarthmore.edu>
2023-10-25 12:40:11 -0400

    dummy commit

--- config.py
+++ config.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
-
 class Config(object):
     """Configuration object"""
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'really-secret-key'
